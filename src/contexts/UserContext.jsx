f0e4dbaf3a48d3be7c12eaeec7613407cdd1d1b8
Ezequiel Ramirez <ezequielram@gmail.com>
2021-09-29 17:36:02 -0300

    stast powerstats

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -8,8 +8,58 @@ const initialUser = { id: 1, name: "Alkemy", heroesAdded: []}
 const UserProvider = ( {children}) => {
 
     const [user, setUser] = useState(initialUser);
+    const [totalStats, setTotalStats] = useState([]);
     const {heroesAdded} = user;
     
+
+    const handleStats = (hero) => {
+        
+          const handleCombat =
+            parseInt(totalStats.combat) + parseInt(hero?.powerstats?.combat);
+          const handleDurability =
+            parseInt(totalStats.durability) + parseInt(hero?.durability);
+          const handleIntelligence =
+            parseInt(totalStats.intelligence) + parseInt(hero.powerstats?.intelligence);
+          const handlePower = parseInt(totalStats.power) + parseInt(hero?.powerstats?.power);
+          const handleSpeed = parseInt(totalStats.speed) + parseInt(hero?.powerstats?.speed);
+          const handleStrength =
+            parseInt(totalStats.strength) + parseInt(hero?.powerstats?.strength);
+          const newStats = {
+            combat: handleCombat.toString(),
+            durability: handleDurability.toString(),
+            intelligence: handleIntelligence.toString(),
+            power: handlePower.toString(),
+            speed: handleSpeed.toString(),
+            strength: handleStrength.toString(),
+          };
+          setTotalStats(newStats);
+        
+      };
+    
+      const deleteStats = (hero) => {
+        const newCombat =
+          parseInt(totalStats.combat) - parseInt(hero.powerstats?.combat);
+        const newDurability =
+          parseInt(totalStats.durability) - parseInt(hero.powerstats?.durability);
+        const newIntelligence =
+          parseInt(totalStats.intelligence) -
+          parseInt(hero.powerstats?.intelligence);
+        const newPower =
+          parseInt(totalStats.power) - parseInt(hero.powerstats?.power);
+        const newSpeed =
+          parseInt(totalStats.speed) - parseInt(hero.powerstats?.speed);
+        const newStrength =
+          parseInt(totalStats.strength) - parseInt(hero.powerstats?.strength);
+        const newStats = {
+          combat: newCombat.toString(),
+          durability: newDurability.toString(),
+          intelligence: newIntelligence.toString(),
+          power: newPower.toString(),
+          speed: newSpeed.toString(),
+          strength: newStrength.toString(),
+        };
+        setTotalStats(newStats);
+      };
 
     const login = () =>{
         setUser(initialUser);
@@ -23,7 +73,7 @@ const UserProvider = ( {children}) => {
         const heroesAdded = isInTeam
         ? user.heroesAdded.filter(favHero => favHero.id !== hero.id) //delete 
         :[...user.heroesAdded, hero] //add
-        setUser({...user, heroesAdded })
+        setUser({...user, heroesAdded });
     }
 //funcion para detectar si se encuentra el heroe en el team y setear el boton de agregar/eliminar
     const isAdded = (hero) =>{
